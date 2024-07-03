992592f13e31b9809b74eff29a24b5edab5e3b1e
aksharkh <aksharkh04@gmail.com>
2024-07-03 19:20:46 +0530

    added front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import MainPage from "./components/mainpage/MainPage";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Landing from "./components/landing/Landing";
+import Navbar from "./components/navbar/Navbar";
+import Form from "./components/form/Form";
 import './App.css';
+import Recommendation from "./components/recommendation/Recommendation";
+import MainpageDetails from "./components/mainpage_details/MainpageDetails";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  console.log(isAuthenticated);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={isAuthenticated ? <MainPage /> : <Landing />}
+          />
+          <Route
+            path="/mainpage"
+            element={isAuthenticated ? <MainPage /> : <Landing />}
+          />
+          {/* <Route path="/mainpage" element={<MainPage />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/form/:userId" element={<Form />} />
+          <Route path="/mainpagedetails/:postId" element={<MainpageDetails />} />
+          <Route path="/recommendation/:questionId/:age/:description" element={<Recommendation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
